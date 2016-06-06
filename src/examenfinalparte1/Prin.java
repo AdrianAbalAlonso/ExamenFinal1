d40add2fb37d3eaf1005c0101feb0a88ced87859
aabalalonso <aabalalonso@atanasoff11.danielcastelao.org>
2016-06-06 09:34:22 +0200

    javadoc in primary class , and finish proyect

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -11,19 +11,19 @@ public class Prin {
         
         CallBuilderLeast1();
     }
-
+//introduced into method CallBuilderLeast1 the builder least 1 to parameter you enter
     private static void CallBuilderLeast1() {
         Secundary mn = new Secundary();
         int nn = mn.ValueToAccesBuilders;
         System.out.println("Es: " + nn);
     }
-
+//introduced into method CallBuilder1parameter the builder output a paremeter you enter
     private static void CallBuilder1paremeter() {
         Secundary nm = new Secundary(10);
         int m = nm.ValueToAccesBuilders;
         System.out.println("Es: " + m);
     }
-
+//introduced into method CallBuilder2parameter the builder sum 2 parameters you enter
     private static void CallBuilder2parameters() {
         Secundary obj = new Secundary(10, 20);
         int n = obj.ValueToAccesBuilders;
