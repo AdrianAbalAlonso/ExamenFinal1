a9e52f49f5cd0667c1d79260e48d147b59fbee0c
aabalalonso <aabalalonso@atanasoff11.danielcastelao.org>
2016-06-06 09:29:22 +0200

    Javadoc of all builders of Secundary class , and the method sum .On the primary class , introduced in methods the calls to builders of secundary class , i think its look more simple

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -5,16 +5,28 @@ import java.io.IOException;
 public class Prin {
 
     public static void main(String[] args) throws IOException {
-        Secundary obj = new Secundary(10, 20);
-        int n = obj.i;
-        System.out.println("Es: " + n);
+        CallBuilder2parameters();
         
-        Secundary nm = new Secundary(10);
-        int m = nm.i;
-        System.out.println("Es: " + m);
+        CallBuilder1paremeter();
         
+        CallBuilderLeast1();
+    }
+
+    private static void CallBuilderLeast1() {
         Secundary mn = new Secundary();
-        int nn = mn.i;
+        int nn = mn.ValueToAccesBuilders;
         System.out.println("Es: " + nn);
+    }
+
+    private static void CallBuilder1paremeter() {
+        Secundary nm = new Secundary(10);
+        int m = nm.ValueToAccesBuilders;
+        System.out.println("Es: " + m);
+    }
+
+    private static void CallBuilder2parameters() {
+        Secundary obj = new Secundary(10, 20);
+        int n = obj.ValueToAccesBuilders;
+        System.out.println("Es: " + n);
     }
 }
--- src/examenfinalparte1/Secundary.java
+++ src/examenfinalparte1/Secundary.java
@@ -2,22 +2,27 @@ package examenfinalparte1;
 
 public class Secundary {
 //the variable must be private type to protect the code
-    public int i;
+    public int ValueToAccesBuilders;
     /**
      * builder with parameters , you need get in main class  values to value1 and value2.
      * @param value1 int type
      * @param value2 int type
      */
     public Secundary(int value1, int value2) {
-        i = sum(value1, value2);
+        ValueToAccesBuilders = sum(value1, value2);
     }
-
+/**
+ * builder with a once parameter,its illogical call the sum method in this builder , because you only enter once parameter.
+ * @param value1 
+ */
     public Secundary(int value1) {
-        i = sum(value1, value1);
+        ValueToAccesBuilders = sum(value1, value1);
     }
-
+/**
+ * Este metodo lo voy a explicar en español mejor , cuando tu no metes nada por parametro , y este constructor resta 1 , dara -1 , como da en la salida
+ */
     public Secundary() {
-        i = -1;
+        ValueToAccesBuilders = -1;
     }
 /**
  * Method to sum the values 
